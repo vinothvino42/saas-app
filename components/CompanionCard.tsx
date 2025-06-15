861a0fdea6ade540376d8828a851edd31e17b2fe
vinothvino42 <vinothvino42@gmail.com>
2025-06-15 21:43:54 +0530

    refactor: add bookmark functionality to companion card component

--- components/CompanionCard.tsx
+++ components/CompanionCard.tsx
@@ -1,5 +1,9 @@
+"use client";
+import { removeBookmark } from "@/lib/actions/companion.actions";
+import { addBookmark } from "@/lib/actions/companion.actions";
 import Image from "next/image";
 import Link from "next/link";
+import { usePathname } from "next/navigation";
 
 interface CompanionCardProps {
   id: string;
@@ -8,6 +12,7 @@ interface CompanionCardProps {
   subject: string;
   duration: number;
   color: string;
+  bookmarked: boolean;
 }
 
 const CompanionCard = ({
@@ -17,20 +22,32 @@ const CompanionCard = ({
   subject,
   duration,
   color,
+  bookmarked,
 }: CompanionCardProps) => {
+  const pathname = usePathname();
+  const handleBookmark = async () => {
+    if (bookmarked) {
+      await removeBookmark(id, pathname);
+    } else {
+      await addBookmark(id, pathname);
+    }
+  };
   return (
     <article className="companion-card" style={{ backgroundColor: color }}>
       <div className="flex justify-between items-center">
         <div className="subject-badge">{subject}</div>
-        <button className="companion-bookmark">
+        <button className="companion-bookmark" onClick={handleBookmark}>
           <Image
-            src="/icons/bookmark.svg"
+            src={
+              bookmarked ? "/icons/bookmark-filled.svg" : "/icons/bookmark.svg"
+            }
             alt="bookmark"
             width={12.5}
             height={15}
           />
         </button>
       </div>
+
       <h2 className="text-2xl font-bold">{name}</h2>
       <p className="text-sm">{topic}</p>
       <div className="flex items-center gap-2">
@@ -44,7 +61,9 @@ const CompanionCard = ({
       </div>
 
       <Link href={`/companions/${id}`} className="w-full">
-        Launch Lession
+        <button className="btn-primary w-full justify-center">
+          Launch Lesson
+        </button>
       </Link>
     </article>
   );
