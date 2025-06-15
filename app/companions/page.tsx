8e54c4520f6f84662296c6794f47205bb36b4561
vinothvino42 <vinothvino42@gmail.com>
2025-06-15 21:39:06 +0530

    refactor: complete companion session and create new session page user interface

--- app/companions/page.tsx
+++ app/companions/page.tsx
@@ -1,5 +1,37 @@
-const CompanionsLibraryPage = () => {
-  return <div>CompanionsLibraryPage</div>;
+import CompanionCard from "@/components/CompanionCard";
+import SearchInput from "@/components/SearchInput";
+import SubjectFilter from "@/components/SubjectFilter";
+import { getAllCompanions } from "@/lib/actions/companion.actions";
+import { getSubjectColor } from "@/lib/utils";
+
+const CompanionsLibraryPage = async ({ searchParams }: SearchParams) => {
+  const filters = await searchParams;
+  const subject = filters.subject ? filters.subject : "";
+  const topic = filters.topic ? filters.topic : "";
+
+  const companions = await getAllCompanions({ subject, topic });
+  console.log(companions);
+
+  return (
+    <main>
+      <section className="flex justify-between gap-4 max-sm:flex-col">
+        <h1>Companion Library</h1>
+        <div className="flex gap-4">
+          <SearchInput />
+          <SubjectFilter />
+        </div>
+      </section>
+      <section className="companions-grid">
+        {companions.map((companion) => (
+          <CompanionCard
+            key={companion.id}
+            {...companion}
+            color={getSubjectColor(companion.subject)}
+          />
+        ))}
+      </section>
+    </main>
+  );
 };
 
 export default CompanionsLibraryPage;
