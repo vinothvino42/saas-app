4718d8c197420e4cd8381fbc17cadd506a91a31e
vinothvino42 <vinothvino42@gmail.com>
2025-06-15 10:09:42 +0530

    feat: add companion form for creating a new companions

--- app/companions/new/page.tsx
+++ app/companions/new/page.tsx
@@ -1,5 +1,19 @@
-const NewCompanionPage = () => {
-  return <div>NewCompanionPage</div>;
+import CompanionForm from "@/components/CompanionForm";
+import { auth } from "@clerk/nextjs/server";
+import { redirect } from "next/navigation";
+
+const NewCompanionPage = async () => {
+  const { userId } = await auth();
+  if (!userId) redirect("/sign-in");
+
+  return (
+    <main className="min-lg:w-1/3 min-md:w-2/3 items-center justify-center">
+      <article className="w-full gap-4 flex flex-col">
+        <h1>Companion Builder</h1>
+        <CompanionForm />
+      </article>
+    </main>
+  );
 };
 
 export default NewCompanionPage;
